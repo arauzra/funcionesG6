c151856dfb8e3398c9dd3501aa740a53beb7971f
Jonas <jfehodgson@uamv.edu.ni>
2026-05-25 12:36:40 -0600

    feat: agregue resta

--- funcion1/Program.cs
+++ funcion1/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Crear un menu de opciones para el usuario, 
+//donde pueda elegir entre las siguiente opciones:
+// 1. Sumar dos numeros
+// 2. Restar dos numeros
+int num1, num2, resultado, opcion;
+
+int resta (int num1, int num2)
+{
+    return num1-num2;
+}
+
+// 3. Multiplicar dos numeros
+// 4. Dividir dos numeros
+// 5. Salir
+
+
+
