78e5cad9cf6b756a1de0d9d57e1e872a730546f3
Sergio <sicabrera@uamv.edu.ni>
2026-05-25 12:34:17 -0600

    feat: agregue multiplicacion

--- funcion1/Program.cs
+++ funcion1/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Crear un menu de opciones para el usuario, 
+//donde pueda elegir entre las siguiente opciones:
+// 1. Sumar dos numeros
+// 2. Restar dos numeros
+// 3. Multiplicar dos numeros
+
+int num1, num2, resultado, opcion;
+
+int multiplicacion (int a, int b)
+{
+    return a * b;
+}
+
+// 4. Dividir dos numeros
+// 5. Salir
+
