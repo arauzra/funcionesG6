8b47ae0fc430aca666b3c3a9556757f6534510c1
USUARIO <ervinroch@uamv.edu.ni>
2026-05-25 12:38:14 -0600

    feat: agregue division

--- funcion1/Program.cs
+++ funcion1/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Crear un menu de opciones para el usuario, 
+//donde pueda elegir entre las siguiente opciones:
+// 1. Sumar dos numeros
+// 2. Restar dos numeros
+// 3. Multiplicar dos numeros
+// 4. Dividir dos numeros
+
+int num1, num2, resultado, opcion;
+
+int division (int num1, int num2)
+ {
+
+    return a / b;
+
+ }
+
+// 5. Salir
+
+
