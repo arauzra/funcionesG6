7777311484240792dcec05b33630cc47301c195d
Rafael <arauzra@uamv.edu.ni>
2026-05-25 12:25:51 -0600

    solucion

--- funcion1/Program.cs
+++ funcion1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Crear un menu de opciones para el usuario, 
+//donde pueda elegir entre las siguiente opciones:
+// 1. Sumar dos numeros
+// 2. Restar dos numeros
+// 3. Multiplicar dos numeros
+// 4. Dividir dos numeros
+// 5. Salir
+
+int num1, num2, resultado, opcion;
