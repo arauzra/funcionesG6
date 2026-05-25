f513701c1dbec08ce4cb03d4b15d471fa0c0a0de
Ric <ruizricardogame@gmail.com>
2026-05-25 12:38:56 -0600

    feat: Menu

--- funcion1/Program.cs
+++ funcion1/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Crear un menu de opciones para el usuario, 
+//donde pueda elegir entre las siguiente opciones:
+// 1. Sumar dos numeros
+// 2. Restar dos numeros
+// 3. Multiplicar dos numeros
+// 4. Dividir dos numeros
+// 5. Salir
+
+int num1, num2, resultado, opcion;
+
+//Menu de opciones
+
+console.writeline("Menu de opciones:");
+console.writeline("1. Sumar dos numeros");
+console.writeline("2. Restar dos numeros");
+console.writeline("3. Multiplicar dos numeros");
+console.writeline("4. Dividir dos numeros");
+console.writeline("5. Salir");
+console.writeline("Ingrese una opcion:");
+opcion = int.parse(console.readline());
+
